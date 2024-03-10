2a366ce41f5b561811f5c60385807aaa8394dd98
Ziaaulia <143148235+Ziaaulia@users.noreply.github.com>
2024-03-10 22:48:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'March 10, 2024',
+  birthdate: 'March 12, 2024',
   name: 'Sayang'
 };
 
